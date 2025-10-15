77923be49b81471d6a5ff4db3c23fa22e72ec040
EentErt <nate.giff@gmail.com>
2025-10-14 22:37:06 -0700

    added metrics

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,42 @@
 import express from "express";
+import { config } from "./config.js";
 const app = express();
 const PORT = 8080;
-app.use(express.static("."));
+app.use("/app", middlewareMetricsInc);
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+app.use("/metrics", handlerHits);
+app.use("/reset", handlerReset);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
+app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerHits);
+function handlerReadiness(req, res) {
+    res.set("Content-Type", "text/plain");
+    res.status(200).send("OK");
+    return;
+}
+function handlerHits(req, res) {
+    res.set("Content-Type", "text/plain");
+    res.status(200).send("Hits: " + config.fileserverHits);
+    return;
+}
+function handlerReset(req, res) {
+    config.fileserverHits = 0;
+    res.set("Content-Type", "text/plain");
+    res.status(200).send("OK");
+    return;
+}
+function middlewareLogResponses(req, res, next) {
+    res.on("finish", () => {
+        if (res.statusCode !== 200) {
+            console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+        }
+    });
+    next();
+}
+function middlewareMetricsInc(req, res, next) {
+    config.fileserverHits += 1;
+    next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,52 @@
-import express from "express";
+import express, { Response, Request, NextFunction } from "express";
+import { config } from "./config.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", middlewareMetricsInc);
+app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+app.use("/metrics", handlerHits);
+app.use("/reset", handlerReset);
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
 });
+
+app.get("/healthz", handlerReadiness);
+app.get("/metrics", handlerHits);
+
+function handlerReadiness(req: Request, res: express.Response): void {
+  res.set("Content-Type", "text/plain");
+  res.status(200).send("OK");
+  return;
+}
+
+function handlerHits(req: Request, res: express.Response): void {
+  res.set("Content-Type", "text/plain");
+  res.status(200).send("Hits: " + config.fileserverHits);
+  return;
+}
+
+function handlerReset(req: Request, res: express.Response): void {
+  config.fileserverHits = 0;
+  res.set("Content-Type", "text/plain");
+  res.status(200).send("OK");
+  return;
+}
+
+
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction): void {
+  res.on("finish", () => {
+    if (res.statusCode !== 200) {
+      console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`);
+    }
+  })
+  next();
+}
+
+function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
+  config.fileserverHits += 1;
+  next();
+}
