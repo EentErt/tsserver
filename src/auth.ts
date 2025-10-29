a81285bd56dfc439f30a56c5e03133c0ac0ee1e3
EentErt <nate.giff@gmail.com>
2025-10-28 21:41:26 -0700

    added jwt auth and tests

--- src/auth.ts
+++ src/auth.ts
@@ -1,4 +1,6 @@
 import { verify, hash } from "argon2";
+import jwt, { JwtPayload } from "jsonwebtoken";
+
 
 export async function hashPassword(password: string): Promise<string> {
     const hashedPassword = await hash(password);
@@ -7,4 +9,25 @@ export async function hashPassword(password: string): Promise<string> {
 
 export async function checkPasswordHash(password: string, hash: string): Promise<boolean> {
     return await verify(hash, password);
+}
+
+export function makeJWT(userID: string, expiresIn: number, secret: string): string {
+    type payload = Pick<JwtPayload, "iss" | "sub" | "iat" | "exp">;
+    const load = {
+        iss: "chirpy",
+        sub: userID,
+        iat: Math.floor(Date.now() / 1000),
+        exp: Math.floor(Date.now() / 1000) + expiresIn,
+    }
+    const token = jwt.sign(load, secret, {expiresIn: expiresIn })
+    return token;
+}
+
+export function validateJWT(tokenString: string, secret: string): string {
+    try {
+        const token = jwt.verify(tokenString, secret);
+        return token.sub as string;
+    } catch (error) {
+        throw error;
+    }
 }
